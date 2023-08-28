e33f67bb81c472abfc1f5108da37f738c28be629
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2023-08-28 15:28:35 +0545

    feat: add pypi package files

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # tibia-ocr
 
+OCR to read Tibia in-game text written in python
+
+## Installation
+
+You can install the package via pip:
+
+```bash
+pip install tibia-ocr
+```
+
+## Usage
+
+```python
+import tibia-ocr
+
+# usage examples here
+```
+
 ## Known Limitations
 
 - `!` is detected as `i`
 - Can not detect spaces
+
+## License
+
+This project is licensed under the terms of the MIT license.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## Contact
+
+If you want to contact me you can reach me at pradishbijukchhe@gmail.com.
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tibia-ocr",
+    version="1.0.0",
+    author="Pradish Bijukchhe",
+    author_email="pradishbijukchhe@gmail.com",
+    description="OCR to read Tibia in-game text written in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sandbox-pokhara/tibia-ocr",
+    project_urls={
+        "Bug Tracker": "https://github.com/sandbox-pokhara/tibia-ocr/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    include_package_data=True,
+    package_dir={"tibia-ocr": "tibia-ocr"},
+    python_requires=">=3",
+    install_requires=[],
+)
